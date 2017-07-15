8f2785a26e9e26fe8609ee264211fa962d7772c0
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-07-15 01:54:45 -0400

    add average cost

--- program.js
+++ program.js
@@ -2,9 +2,17 @@
  * Created by benjaminhuang on 7/14/17.
  */
 
-var lunch = "pizza";
+var lunch = "Pizza for Lunch";
 console.log(lunch);
 
 var name = "benjamin";
-name ="Huang";
+name ="Host Benjamin Huang";
 console.log(name);
+
+var totalCost = 14.99;
+
+var sizeOfParty = process.argv[2];
+
+var averageCost = totalCost / sizeOfParty;
+
+console.log(averageCost);
