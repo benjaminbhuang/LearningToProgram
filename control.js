4f5aadbee1d52045e731c10b84cb7b1cb4f67538
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-07-15 16:45:37 -0400

    add array

--- control.js
+++ control.js
@@ -10,8 +10,39 @@ var getDieRoll = function (dieSize) {
 
 var roll = getDieRoll(6);
 
-if (roll >= 5){
-    console.log("You rolled a great rolll "+ roll);
-}else {
-    console.log("You rolled a " + roll);
+while(roll < 4){
+    roll = getDieRoll(6);
+}
+
+do {
+
+    if (roll >= 5) {
+        console.log("You rolled a great rolll " + roll);
+    } else {
+        console.log("You rolled a " + roll);
+    }
+
+    roll = getDieRoll(6);
+
+}while( roll < 3)
+
+var sum = 0;
+var count = 0;
+for( var i =0; i < 10; i ++){
+    roll = getDieRoll(6);
+    sum += roll;
+    count = i;
+}
+
+console.log("Your average roll: "+sum/count);
+
+//array
+var things =[1,2,3,4,7,9, "pizza"];
+
+for(var i=0; i < things.length; i++){
+    console.log(things[i]);
+}
+
+for(var i = 0; i < process.argv.length; i++){
+    console.log(process.argv[i]);
 }
